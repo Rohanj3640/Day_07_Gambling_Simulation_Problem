89477da4f50325768c08bd2e106be395bb9a8282
Rohan Jadhav <rohanj121999@gmail.com>
2021-12-08 00:17:49 +0530

    Added: welcome message

---
+++ src/com/bridgelabs/GamblingProblem.java
@@ -0,0 +1,12 @@
+package com.bridgelabs;
+
+public class GamblingProblem {
+    //UC1 TAKING STAKE AS 100 AND BET AS 1
+
+    int STAKE = 100;
+    int BET = 1;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Gambling Simulation Problem");
+    }
+}
