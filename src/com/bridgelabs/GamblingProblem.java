dc5abbbdb5b1599f10519ed6b1743b815d295c83
Rohan Jadhav <rohanj121999@gmail.com>
2021-12-08 00:46:02 +0530

    Modified: Checking Win or Loss

--- src/com/bridgelabs/GamblingProblem.java
+++ src/com/bridgelabs/GamblingProblem.java
@@ -3,10 +3,27 @@ package com.bridgelabs;
 public class GamblingProblem {
     //UC1 TAKING STAKE AS 100 AND BET AS 1
 
-    int STAKE = 100;
-    int BET = 1;
+    public static final int STAKE = 100;
+    public static final int BET = 1;
+
+    //UC2 CHECK WIN OR LOSS
+
+    public void makeBet() {
+
+        int win = 1;
+        int random = (int) (Math.random() * 2);
+
+        if (random == win) {
+            System.out.println("WON");
+        } else {
+            System.out.println("LOSE");
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Gambling Simulation Problem");
+
+        GamblingProblem game = new GamblingProblem();
+        game.makeBet();
     }
 }
